b155b3759b3f9f95471c37fa554295c7e3f52b16
vly0 <86524769+vly0@users.noreply.github.com>
2021-12-08 21:44:58 +0200

    adaptive

--- js/script.js
+++ js/script.js
@@ -26,10 +26,19 @@ $(document).ready(function () {
         slidesToScroll: 3,
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2
+                }
+            },
+
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
         ]
